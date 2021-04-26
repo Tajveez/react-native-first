e1c45743f25d25a8c670f40710add0d877910e86
tajveez <tajveez786@gmail.com>
2021-04-26 21:19:24 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  console.log('Logging...')
+  return (
+    <View style={styles.container}>
+      <Button color="orange" title="Log in" onPress={() => alert('Yes!!!')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center"
+  },
+});
